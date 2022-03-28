9538da88ada5e509d606856053ec84e1ddc631ef
user42 <psaumet@student.42.fr>
2022-03-28 16:16:49 +0200

    Adding main.cpp

---
+++ CPP01/ex06/main.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psaumet <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/01 11:07:34 by psaumet           #+#    #+#             */
+/*   Updated: 2021/06/01 11:07:42 by psaumet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Weapon.hpp"
+#include "HumanA.hpp"
+#include "HumanB.hpp"
+
+/*
+**	clang++ -Wall -Werror -Wextra -std=c++98 -o main main.cpp Weapon.cpp HumanA.cpp HumanB.cpp && ./main
+*/
+
+int			main( void )
+{
+	{
+		Weapon	club = Weapon("crude spiked club");
+
+		HumanA	bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+
+	{
+		Weapon		club = Weapon("crude spiked club");
+
+		HumanB		jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
+	return (0);
+}

aadb9a8fd237151e543733d226dfbb60d7f0ee66
user42 <psaumet@student.42.fr>
2022-03-28 16:11:57 +0200

    Adding Zombie.cpp

---
+++ CPP01/ex02/Zombie.cpp
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psaumet <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/01 11:07:34 by psaumet           #+#    #+#             */
+/*   Updated: 2021/06/01 11:07:42 by psaumet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+/*
+**  Zombie's constructor
+*/
+Zombie::Zombie( std::string name, std::string type) :
+ _name(name), _type(type)
+{
+    _is_speaking = "Braiiiiiiinnnssss ...";
+	Zombie::_nbInst += 1;
+    return ;
+}
+
+/*
+**  Zombie's copy constructor
+*/
+Zombie::Zombie( const Zombie &copy )
+{
+    std::cout << "Zombie's copy constructor is initialized!" << std::endl;
+    _name = copy._name;
+    _type = copy._type;
+    return ;
+}
+
+/*
+**  Zombie's Destructor
+*/
+Zombie::~Zombie( void )
+{
+	Zombie::_nbDelInst += 1;
+	std::cout << "Zombies killed : " << Zombie::getNbDelInst() << std::endl;
+    return ;
+}
+
+/*
+**  Zombie's copy of assignement
+*/
+Zombie &Zombie::operator=( const Zombie &element)
+{
+    std::cout << "Operator assignement : " << &element << std::endl;
+    return (*this);
+}
+
+/* *************************** */
+/*  Zombie's Public functions  */
+/* *************************** */
+
+void        Zombie::announce( void )
+{
+    std::cout << "<" << this->_name << " (" << this->_type << ")> " << this->_is_speaking << std::endl; 
+}
+
+int			Zombie::getNbInst( void )
+{
+	return (Zombie::_nbInst);
+}
+
+int			Zombie::getNbDelInst( void )
+{
+	return (Zombie::_nbDelInst);
+}
+/*
+**	Attribut non-member
+*/
+int			Zombie::_nbInst = 0;
+int			Zombie::_nbDelInst = 0;

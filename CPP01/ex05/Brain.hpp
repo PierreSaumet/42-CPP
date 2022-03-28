fceb4abf2702fd374a4bfca660e95339821bfcf6
user42 <psaumet@student.42.fr>
2022-03-28 16:15:02 +0200

    Adding Brain.hpp

---
+++ CPP01/ex05/Brain.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Brain.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psaumet <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/01 11:07:34 by psaumet           #+#    #+#             */
+/*   Updated: 2021/06/01 11:07:42 by psaumet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BRAIN_HPP
+# define BRAIN_HPP
+
+#include <iostream>
+#include <sstream>
+
+class	Brain {
+
+	public:
+
+		Brain ( void );
+		Brain ( const Brain& );
+		~Brain ( void );
+		Brain &operator=( const Brain &element );
+
+		std::string		identify( void );
+
+		std::string			get_area( void );
+		void				set_area( std::string area );
+
+	private:
+
+		std::string		_area;
+};
+
+#endif

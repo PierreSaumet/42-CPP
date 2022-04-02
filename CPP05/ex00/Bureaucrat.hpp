81ce96b9af7b46d1737cadd08c6b3febf28d2be6
user42 <psaumet@student.42.fr>
2022-04-02 02:06:50 +0200

    Adding Bureaucrate.hpp

---
+++ CPP05/ex00/Bureaucrat.hpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psaumet <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/01 11:07:34 by psaumet           #+#    #+#             */
+/*   Updated: 2021/06/01 11:07:42 by psaumet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <exception>
+
+class Bureaucrat
+{
+	public:
+		//Canonique form
+		Bureaucrat( std::string const &name, int grade );
+		virtual ~Bureaucrat( void );
+		Bureaucrat( const Bureaucrat &copy );
+
+		// Overload operator
+		Bureaucrat		&operator=( const Bureaucrat &element );
+
+		// Getter
+		std::string const		&getName( void ) const;
+		int						getGrade( void ) const;
+
+		// Other functions
+		void					incGrade( void );
+		void					decGrade( void );					
+
+		// Overload class exception
+		class GradeTooHighException : public std::exception
+		{
+			virtual const char *what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			virtual const char *what() const throw();
+		};
+
+
+
+	private:
+		Bureaucrat( void );
+		std::string 			_name;
+		int						_grade;
+
+};
+
+std::ostream &operator<<( std::ostream &o, Bureaucrat const &rhs );
+
+#endif
